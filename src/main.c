ff7d3b1a17ea5931feaad5e38a8f2b78d9afcbe6
6z7y <eenanho24n@proton.me>
2026-03-26 22:13:52 +0300

    improve main code

--- src/main.c
+++ src/main.c
@@ -1,74 +1,52 @@
 #include <ncurses.h>
 #include <unistd.h>
-#include "Episodes/include_helper.h"
+
 #include "GAME_DATA.h"
 #include "control.h"
-// #include "Episodes/include_helper.h"
+#include "drwing.h"
 #include "menu.h"
 #include "utils.h"
-#include "drwing.h"
-
-void setup_game(GAME_Context *game_ctx)
-{
-  // game on
-  game_ctx->game_running = 1;
-
-  // init window
-  game_ctx->w[0] = newwin(WG_HEIGHT, WG_WIDTH, WG_Y, WG_X);
-  game_ctx->w[1] = newwin(WI_HEIGHT, WI_WIDTH, WI_Y, WI_X);
-
-  // episode select
-  // 3. select episode
-  game_ctx->ep = select_episode();
-
-  // load map FIRST, then setup player
-  load_episode1(&game_ctx->map);
-  setup_user_for_ep(&game_ctx->player, &game_ctx->ep, &game_ctx->map);
-  for (int i = 0; i < 5; i++) game_ctx->player.inventory[i] = '.';
-}
 
 GAME_Context game_ctx = {0};
 
 int main(void) 
 {
-    int c; // key
-    int y, x; // position
-    int frame     = 0; // for animation T
-    int anim_tick = 0;
+  int c; // key
+  int y, x; // position
+  int frame     = 0; // for animation T
+  int anim_tick = 0;
 
-    // 1 start game
-    ncurses_mode(1); // init ncurses
-                     //
-    // 2. setup game
-    setup_game(&game_ctx);
+  ncurses_mode(1); // init ncurses
 
-    while (game_ctx.game_running) {
-        getmaxyx(stdscr, y, x);
+  // 1. setup game
+  setup_game(&game_ctx);
 
-        // 5. check terminal size
-        if (x < 40 || y < 40) {
-            warn_term_size(y, x);
-            continue;
-        }
+  while (game_ctx.game_running) {
+      getmaxyx(stdscr, y, x);
 
-        // 6. control
-        c = getch();
-        if (handle_control(c, &game_ctx.player, &game_ctx.map) == 1) break;
+      if (x < 40 || y < 40) {
+        warn_term_size(y, x);
+        continue;
+      }
 
-        // 7. drawing
-        render_game(game_ctx.w[0], game_ctx.w[1], game_ctx.ep, &game_ctx.player, frame, &game_ctx.map);
+      // 2. input handle
+      c = getch();
+      if (handle_control(c, &game_ctx.player, &game_ctx.map) == 1) break;
 
-        // 8. refreshing
-        wnoutrefresh(stdscr);
-        wnoutrefresh(game_ctx.w[0]);
-        wnoutrefresh(game_ctx.w[1]);
-        doupdate();
+      // 4. ncurses drawing
+      render_game(game_ctx.w[0], game_ctx.w[1], game_ctx.ep, &game_ctx.player, frame, &game_ctx.map);
 
-        usleep(16000);  /* 60fps */
+      // 4. update window
+      wnoutrefresh(stdscr);
+      wnoutrefresh(game_ctx.w[0]);
+      wnoutrefresh(game_ctx.w[1]);
+      doupdate();
 
-        update_T_animation(&anim_tick, &frame);
-    }
+      usleep(16000);  /* 60fps */
 
-    ncurses_mode(0);
-    return 0;
+      update_T_animation(&anim_tick, &frame);
+  }
+
+  ncurses_mode(0); // close ncurses
+  return 0;
 }
