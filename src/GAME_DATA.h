7bfa0718cb3c9940efeab383794bfd6d25ac22b4
6z7y <eenanho24n@proton.me>
2026-03-28 01:05:17 +0300

    add gamestate & noval

--- src/GAME_DATA.h
+++ src/GAME_DATA.h
@@ -7,15 +7,15 @@
 #define TILE_WALL '#'
 #define TILE_CAGE '|'
 
-#define TILE_ITEM1 'C'    // chocolate
-#define TILE_ITEM2 'j'    // chocolate
+#define TILE_ITEM1 'C'
+#define TILE_ITEM2 'K'
 
 #define TILE_EXIT1 'E' 
 #define TILE_EXIT2 'X'
 #define TILE_EXIT3 'I'
 
 // how much window
-#define NUM_WINDOW 2
+#define NUM_WINDOW 3
 
 // WINDOW GAME
 #define WG_HEIGHT 35
@@ -29,10 +29,33 @@
 #define WIC_Y 40
 #define WIC_X 40
 
-#define WIO_HEIGHT 3
-#define WIO_WIDTH 21
-#define WIO_Y 40
-#define WIO_X 40
+#define WIO_HEIGHT 14
+#define WIO_WIDTH 37
+#define WIO_Y 38
+#define WIO_X 36
+
+// WINDOW Novel (right side)
+#define WN_HEIGHT 40
+#define WN_WIDTH  70
+#define WN_Y      2
+#define WN_X      95
+
+// #define NOVAL_CHAR_Y 15
+// #define NOVAL_CHAR_X 140
+
+typedef enum {
+  CHAR_T,   // Player
+  CHAR_W,   // kanojo T
+  CHAR_H,   // Boss EP1
+  CHAR_F    // friend
+
+} Noval_Character;
+
+typedef enum {
+  STATE_GAME,
+  STATE_CUTSCENE,
+  STATE_MENU,
+} Game_State;
 
 // for episode
 typedef enum {
@@ -72,7 +95,8 @@ typedef struct {
 // structure context
 typedef struct {
   int game_running;
-  WINDOW *w[NUM_WINDOW];
+  Game_State state;
+  WINDOW *wind[NUM_WINDOW];
   MAP_Structure map;
   Episode ep;
   Player player;
