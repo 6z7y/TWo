74b03434f8a6e9a748e786e0c3c921ec7d373e10
6z7y <eenanho24n@proton.me>
2026-03-29 00:00:46 +0300

    add ep2

--- src/Episodes/EP2/ep.h
+++ src/Episodes/EP2/ep.h
@@ -1,7 +1,8 @@
-#ifndef EP_H
-#define EP_H
+#ifndef EP2_H
+#define EP2_H
 
 #include "../../GAME_DATA.h"
 
-#endif
+void load_episode2(MAP_Structure *map);
 
+#endif
---
+++ src/Episodes/EP2/render_map.h
@@ -0,0 +1,11 @@
+
+#ifndef EPISODES_H
+#define EPISODES_H
+
+#include "ep.h"
+
+/* each episode has one function that fills the Map */
+void load_episode2(MAP_Structure *map);
+
+#endif
+
