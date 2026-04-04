bae6d9990213090daf85d0f74766439d3e716324
6z7y <eenanho24n@proton.me>
2026-04-04 19:03:41 +0300

    remove unused code

--- src/main.c
+++ src/main.c
@@ -19,16 +19,10 @@ int main(void)
 
   ncurses_mode(1); // init ncurses
 
-  menu_section();
-
+  // menu_section();
   
   // 1. setup game
   setup_game(&game_ctx);
-
-  // // if (game_ctx.ep == EP1_KIDNAPPING)
-  //     ep1_play_opening(game_ctx.wind[0], game_ctx.wind[1],
-  //                      &game_ctx.player, &game_ctx.map);
-  // game_ctx.state = STATE_GAME;
 
   while (game_ctx.game_running) {
       getmaxyx(stdscr, y, x);
@@ -43,22 +37,8 @@ int main(void)
       // 2. input handle
       if (handle_control(c, &game_ctx.player, &game_ctx.map) == 1) break;
 
-      // // ── check triggers after every move ──
-      // if (game_ctx.ep == EP1_KIDNAPPING) {
-      //   ep1_check_triggers(
-      //       game_ctx.wind[0], game_ctx.wind[1],
-      //       &game_ctx.player, &game_ctx.map
-      //   );
-      // } else if (game_ctx.ep == EP2_PRISON_BREAK) {
-      //
-      // } else if (game_ctx.ep == EP3_BOSS_FIGHT) {
-      //
-      // } else if (game_ctx.ep == EP4_HAPPY_ENDING) {
-
-      // }
-
       // 3. ncurses drawing
-      render_game(game_ctx.wind[0], game_ctx.wind[2], game_ctx.ep, &game_ctx.player, &game_ctx.map);
+      render_game(game_ctx.ep, &game_ctx.player, &game_ctx.map);
 
       // 4. update window
       wnoutrefresh(stdscr);
